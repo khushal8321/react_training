9e050bfa83df32a7fcdcd1b871618cb1ea0feca1
khushal <khushal.patel@bacancy.com>
2023-07-12 20:37:19 +0530

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,110 +1,137 @@
-import { useReducer, useState } from 'react';
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'addtodo':
-      return {
-        todos: [...state.todos, { text: action.text, status: false }],
-        todoCount: state.todoCount + 1
-      };
-    case 'deletetodo':
-      return {
-        todos: state.todos.filter((_, index) => index !== action.index),
-        todoCount: state.todoCount - 1
-      };
-    case 'edittodo':
-      return {
-        todos: state.todos.map((todo, index) =>
-          index === action.index ? { ...todo, text: action.text } : todo
-        ),
-        todoCount: state.todoCount
-      };
-    case 'status':
-      return {
-        todos: state.todos.map((todo, index) =>
-          index === action.index ? { ...todo, status: !todo.status } : todo
-        ),
-        todoCount: state.todoCount
-      };
-    default:
-      return state;
-  }
-}
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
 
 function App() {
-  const [{ todos, todoCount }, dispatch] = useReducer(reducer, {
-    todos: [],
-    todoCount: 0
-  });
-  const [text, setText] = useState('');
-  const [editIndex, setEditIndex] = useState(-1);
+  const [myData, myUserData] = useState([])
+  const [editData, myEditData] = useState(null)
 
-  const deletetodo = index => {
-    dispatch({ type: 'deletetodo', index });
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = () => {
+    axios.get("https://reqres.in/api/users?page=2")
+      .then((res) => {
+        myUserData(res.data.data)
+      })
+      .catch((error) => {
+        console.log("error");
+      });
   };
 
-  const edittodo = (index, text) => {
-    setEditIndex(index);
-    setText(text);
+  const deleteUser = (id) => {
+    console.log(`Delete user with id ${id}`);
+    axios.delete(`https://reqres.in/api/users/${id}`)
+      .then(() => {
+        myUserData((oldData) => oldData.filter((user) => user.id !== id));
+      })
+      .catch((error) => {
+        console.log("error");
+      });
   };
 
-  const saveEdit = index => {
-    dispatch({ type: 'edittodo', index, text });
-    setEditIndex(-1);
-    setText('');
+  const editUser = (id) => {
+    console.log(`Edit user with id ${id}`);
+    axios.get(`https://reqres.in/api/users/${id}`)
+      .then((res) => {
+        myEditData(res.data.data);
+      })
+      .catch((error) => {
+        console.log("error");
+      });
   };
 
-  const toggleStatus = index => {
-    dispatch({ type: 'status', index });
+  const saveUser = () => {
+    if (editData) {
+      console.log(`Saving user with id ${editData.id}`);
+      axios.put(`https://reqres.in/api/users/${editData.id}`, editData)
+        .then(() => {
+          myEditData(null);
+          fetchData();
+        })
+        .catch((error) => {
+          console.log("error");
+        });
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    myEditData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const createUser = () => {
+    const newUser = {
+      id:111,
+      email: "abc@email.com",
+      first_name: "abhi",
+      last_name: "patel",
+      avatar: "https://randomuser.me/api/portraits/men/1.jpg",
+    };
+    myUserData((oldData) => [newUser, ...oldData]);
   };
 
   return (
     <>
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          if (editIndex !== -1) {
-            saveEdit(editIndex);
-          } else {
-            dispatch({ type: 'addtodo', text });
-          }
-          setText('');
-        }}
-      >
-        <input
-          type="text"
-          value={text}
-          onChange={e => setText(e.target.value)}
-        />
-        <button type="submit">{editIndex !== -1 ? 'Save' : 'Add'}</button>
-      </form>
-
-      {todos.map((todo, index) => (
-        <div key={index}>
-          {editIndex === index ? (
-            <input
-              type="text"
-              value={text}
-              onChange={e => setText(e.target.value)}
-            />
-          ) : (
-            <div>{todo.text}</div>
-          )}
+      <h1>hello</h1>
+      <button onClick={createUser}>Create User</button>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Avatar</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {myData.map((udata) => {
+            const { id, email, first_name, last_name, avatar } = udata;
+            return (
+              <tr key={id}>
+                <td>{id}</td>
+                <td>{email}</td>
+                <td>{first_name}</td>
+                <td>{last_name}</td>
+                <td>
+                  <img src={avatar} alt="avatar" />
+                </td>
+                <td>
+                  <button onClick={() => editUser(id)}>Edit</button>
+                </td>
+                <td>
+                  <button onClick={() => deleteUser(id)}>Delete</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      {editData && (
+        <div>
+          <h2>Edit User</h2>
           <input
-            type="checkbox"
-            checked={todo.status}
-            onChange={() => toggleStatus(index)}
+            type="text"
+            name="first_name"
+            value={editData.first_name}
+            onChange={handleChange}
           />
-          {editIndex === index ? (
-            <button onClick={() => saveEdit(index)}>Save</button>
-          ) : (
-            <>
-              <button onClick={() => deletetodo(index)}>Delete</button>
-              <button onClick={() => edittodo(index, todo.text)}>Edit</button>
-            </>
-          )}
+          <input
+            type="text"
+            name="last_name"
+            value={editData.last_name}
+            onChange={handleChange}
+          />
+          <button onClick={saveUser}>Save</button>
         </div>
-      ))}
+      )}
     </>
   );
 }
